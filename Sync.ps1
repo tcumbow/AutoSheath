cedad099f7c0bbd3e15c3e709806f0c5dca3bf1c
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 17:32:27 -0500

    folderized

---
+++ Sync.ps1
@@ -0,0 +1,5 @@
+unison .\AutoSheath A:\AutoSheath -auto -batch
+pause
+unison .\AutoSheath R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoSheath -auto -batch
+pause
+unison .\AutoSheath "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoSheath" -auto -batch
