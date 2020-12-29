c44701a45a72a299ff55bfe1e16b3afc5a9e8a85
Tom Cumbow <tcumbow@icloud.com>
2020-12-29 18:28:16 -0500

    new deploy model

---
+++ Deploy.ps1
@@ -0,0 +1,2 @@
+robocopy .\AutoSheath R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoSheath /mir
+robocopy .\AutoSheath "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoSheath" /mir
--- Sync.ps1
+++
@@ -1,5 +0,0 @@
-unison .\AutoSheath A:\AutoSheath -auto -batch
-pause
-unison .\AutoSheath R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoSheath -auto -batch
-pause
-unison .\AutoSheath "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoSheath" -auto -batch
