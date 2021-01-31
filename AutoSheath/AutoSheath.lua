94799faaef7d56e7fde59ba701ce2deaa2cdd099
Tom Cumbow <tcumbow@icloud.com>
2021-01-31 16:31:01 -0500

    DismissTwilight

--- AutoSheath/AutoSheath.lua
+++ AutoSheath/AutoSheath.lua
@@ -8,7 +8,27 @@ local function SheathWeapon()
 	end
 end
 
-local SecondsToDebounceCombatState = 10
+local function DismissTwilight()
+	-- All the abilityIDs for Twilights
+	local PetList = { 24613, 30581, 30584, 30587, 24636, 30592, 30595, 30598, 24639, 30618, 30622, 30626 }
+
+	local i, k, v
+
+	-- Walk through the player's active buffs
+	for i = 1, GetNumBuffs("player") do
+		local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff = GetUnitBuffInfo("player", i)
+		-- Compare each buff's abilityID to the list of IDs we were given
+		for k, v in pairs(PetList) do
+			if abilityId == v then
+				-- Cancel the buff if we got a match
+				CancelBuff(buffSlot)
+			end
+		end
+	end
+
+end
+
+local SecondsToDebounceCombatState = 20
 
 local ConsecutiveNotInCombatResults = 0
 
@@ -22,6 +42,7 @@ local function OnEventCombatStateChange(_, inCombat)
 
 	if ConsecutiveNotInCombatResults >= SecondsToDebounceCombatState then
 		SheathWeapon()
+		DismissTwilight()
 	end
 end
 
